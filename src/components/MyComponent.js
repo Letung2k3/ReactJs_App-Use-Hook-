76efdc8c40c40c1b8688ce35f54a9856af8971d1
Le Thanh Tung <letung767768@gmail.com>
2024-07-17 17:00:27 +0700

    Video 2:State

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,17 @@
 import React from "react";
 class MyComponent extends React.Component {
+        state = {
+                name: "le Thanh Tung",
+                age: 16,
+                id: "000VCM1"
+        }
         render() {
+                let { name, age, id } = this.state
                 return (
-                        <div>Hello class Component!</div>
+                        <div>
+                                Hello class Component! {id} -
+                                {name} - {age}
+                        </div>
                 )
         }
 }
