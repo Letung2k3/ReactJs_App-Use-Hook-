91911407da97b901a1857ac5fed800a000b9bb79
Le Thanh Tung <letung767768@gmail.com>
2024-07-23 18:21:17 +0700

    Video 7:Hook

--- src/App.js
+++ src/App.js
@@ -3,21 +3,29 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import MyComponent from './components/MyComponent';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-        <MyComponent />
-        {/* <div>Count = {count}</div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Routes>
+            <Route path="/" element={<MyComponent />}>
+            </Route>
+            <Route path="/Home" element={<Home />}>
+            </Route>
+          </Routes>
+          {/* <div>Count = {count}</div>
         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button> */}
-      </header>
-    </div>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -1,59 +1,110 @@
-import React from "react";
+import React, { useState } from "react";
 import '../Assets/UserInfo.scss'
-class UserInfo extends React.Component {
-        state = {
-                name: "Dotatq",
-                age: 16,
+// class UserInfo extends React.Component {
+//         state = {
+//                 name: "Dotatq",
+//                 age: 16,
+//         }
+
+//         handleOnChangeInputName = (event) => this.setState({
+//                 name: event.target.value,
+
+//         })
+//         handleOnChangeInputAge = (Event) => this.setState({
+
+//                 age: Event.target.value
+//         })
+
+//         handleSubmit = (Event) => {
+//                 Event.preventDefault();
+//         }
+//         handleAddUser = (event) => {
+//                 this.props.addUser({
+//                         id: Math.floor(Math.random() * 1000) + 1,
+//                         name: this.state.name,
+//                         age: this.state.age
+//                 })
+//         }
+//         render() {
+//                 let { name, age } = this.state
+//                 return (
+//                         <div className="container">
+//                                 <form onClick={(event) => this.handleSubmit(event)} className="form_submit">
+//                                         <label className="label_name">Name  &nbsp;</label>
+//                                         <input
+//                                                 className="form_input"
+//                                                 value={name}
+//                                                 type="text"
+//                                                 onChange={(Event) => this.handleOnChangeInputName(Event)}
+//                                                 placeholder="Input name..."
+//                                         />
+//                                         &nbsp;
+//                                         &nbsp;
+//                                         <label className="label_name">Age  &nbsp;</label>
+//                                         <input
+//                                                 className="form_input"
+//                                                 value={age}
+//                                                 type="text"
+//                                                 onChange={(Event) => this.handleOnChangeInputAge(Event)}
+//                                                 placeholder="Input age..."
+//                                         />
+//                                         &nbsp;
+//                                         <button className="form_btn" onClick={(event) => this.handleAddUser(event)}>Add</button>
+//                                 </form>
+//                         </div>
+
+//                 )
+//         }
+// }
+const UserInfo = (props) => {
+        let { addUser } = props;
+        const [name, setName] = useState('');
+        const [age, setAge] = useState(0);
+        const handleOnChangeInputName = (event) => {
+                setName(event.target.value)
         }
 
-        handleOnChangeInputName = (event) => this.setState({
-                name: event.target.value,
+        const handleOnChangeInputAge = (Event) => {
+                setAge(Event.target.value)
+        }
 
-        })
-        handleOnChangeInputAge = (Event) => this.setState({
 
-                age: Event.target.value
-        })
-
-        handleSubmit = (Event) => {
+        const handleSubmit = (Event) => {
                 Event.preventDefault();
         }
-        handleAddUser = (event) => {
-                this.props.addUser({
+        const handleAddUser = (event) => {
+                addUser({
                         id: Math.floor(Math.random() * 1000) + 1,
-                        name: this.state.name,
-                        age: this.state.age
+                        name: name,
+                        age: age
                 })
         }
-        render() {
-                let { name, age } = this.state
-                return (
-                        <div className="container">
-                                <form onClick={(event) => this.handleSubmit(event)} className="form_submit">
-                                        <label className="label_name">Name  &nbsp;</label>
-                                        <input
-                                                className="form_input"
-                                                value={name}
-                                                type="text"
-                                                onChange={(Event) => this.handleOnChangeInputName(Event)}
-                                                placeholder="Input name..."
-                                        />
-                                        &nbsp;
-                                        &nbsp;
-                                        <label className="label_name">Age  &nbsp;</label>
-                                        <input
-                                                className="form_input"
-                                                value={age}
-                                                type="text"
-                                                onChange={(Event) => this.handleOnChangeInputAge(Event)}
-                                                placeholder="Input age..."
-                                        />
-                                        &nbsp;
-                                        <button className="form_btn" onClick={(event) => this.handleAddUser(event)}>Add</button>
-                                </form>
-                        </div>
+        return (
+                <div className="container">
+                        <form onClick={(event) => handleSubmit(event)} className="form_submit">
+                                <label className="label_name">Name  &nbsp;</label>
+                                <input
+                                        className="form_input"
+                                        value={name}
+                                        type="text"
+                                        onChange={(Event) => handleOnChangeInputName(Event)}
+                                        placeholder="Input name..."
+                                />
+                                &nbsp;
+                                &nbsp;
+                                <label className="label_name">Age  &nbsp;</label>
+                                <input
+                                        className="form_input"
+                                        value={age}
+                                        type="text"
+                                        onChange={(Event) => handleOnChangeInputAge(Event)}
+                                        placeholder="Input age..."
+                                />
+                                &nbsp;
+                                <button className="form_btn" onClick={(event) => handleAddUser(event)}>Add</button>
+                        </form>
+                </div>
 
-                )
-        }
+        )
 }
 export default UserInfo;
