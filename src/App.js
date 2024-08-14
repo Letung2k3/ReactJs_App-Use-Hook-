e3ec1fa9ad4ea7e0ef35b23dbc7558b77d19f6d1
Le Thanh Tung <letung767768@gmail.com>
2024-08-14 13:36:32 +0700

    test redux

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
 import { Routes, Route } from 'react-router-dom';
 const App = () => {
+  const count = useSelector(state => state.counter.count)
+  //stateRedux => call reducer => state's reducer
+  const dispatch = useDispatch();
   return (
     <div className="container">
       <div className='text-center'>Hello world</div>
+      <div>Count ={count}</div>
+      <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+      <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
     </div>
   );
 }
